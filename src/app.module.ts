402022f41ac897738d93736b101f5d9d0cdd34ce
Svetlana Korbut <tebba@yandex.ru>
2023-08-14 22:49:36 +0200

    feat:migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,43 @@
+// import { Module } from '@nestjs/common';
+// import { UserModule } from './user/user.module';
+// import { ArtistModule } from './artist/artist.module';
+// import { AlbumModule } from './album/album.module';
+// import { TrackModule } from './track/track.module';
+// import { FavsModule } from './favs/favs.module';
+
+// import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './user/user.entity';
+// import { Artist } from './artist/artist.entity';
+// import { Track } from './track/track.entity';
+// import { Album } from './album/album.entity';
+// import { ArtistsFavs } from './favs/artists-to-favs.entity';
+// import { AlbumsFavs } from './favs/albums-to-favs.entity';
+// import { TracksFavs } from './favs/tracks-to-favs.entity';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       envFilePath: '.env'
+//     }),
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: process.env.POSTGRES_HOST,
+//       port: Number(process.env.POSTGRESS_PORT),
+//       username: process.env.POSTGRES_USER,
+//       password: process.env.POSTGRES_PASSWORD,
+//       database: process.env.POSTGRES_DB,
+//       entities: [User, Artist, Track, Album, ArtistsFavs, AlbumsFavs, TracksFavs],
+//       synchronize: true,
+//     }),
+//     UserModule, ArtistModule, AlbumModule, TrackModule, FavsModule,
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
+// export class AppModule { }
+
+
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
@@ -5,31 +45,12 @@ import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavsModule } from './favs/favs.module';
 
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
-import { Artist } from './artist/artist.entity';
-import { Track } from './track/track.entity';
-import { Album } from './album/album.entity';
-import { ArtistsFavs } from './favs/artists-to-favs.entity';
-import { AlbumsFavs } from './favs/albums-to-favs.entity';
-import { TracksFavs } from './favs/tracks-to-favs.entity';
+import { databaseConfig } from './database.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env'
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRESS_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [User, Artist, Track, Album, ArtistsFavs, AlbumsFavs, TracksFavs],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(databaseConfig.options),
     UserModule, ArtistModule, AlbumModule, TrackModule, FavsModule,
   ],
   controllers: [],
